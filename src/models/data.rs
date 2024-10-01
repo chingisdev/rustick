2b91b87c9e926bc2a8c826436b56d5b1523f5997
Chingis <chingisdev@gmail.com>
2024-10-01 14:42:57 +0300

    Refactor data structures to use ndarray for numerical arrays
    Converted `Vec<f64>` instances to `Array1<f64>` and `Array2<f64>` from the ndarray crate for `InputData` and `OutputData`. This change enhances performance and interoperability with numerical computations commonly used in scientific and data processing applications.

--- src/models/data.rs
+++ src/models/data.rs
@@ -1,12 +1,14 @@
+use ndarray::{Array1, Array2};
+
 pub struct InputData {
-    pub open: Option<Vec<f64>>,
-    pub high: Option<Vec<f64>>,
-    pub low: Option<Vec<f64>>,
-    pub close: Option<Vec<f64>>,
-    pub volume: Option<Vec<f64>>,
+    pub open: Option<Array1<f64>>,
+    pub high: Option<Array1<f64>>,
+    pub low: Option<Array1<f64>>,
+    pub close: Option<Array1<f64>>,
+    pub volume: Option<Array1<f64>>,
 }
 
 pub enum OutputData {
-    SingleSeries(Vec<f64>),
-    MultiSeries(Vec<Vec<f64>>)
+    SingleSeries(Array1<f64>),
+    MultiSeries(Array2<f64>)
 }
