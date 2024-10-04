36dfbe4cf3ca650ccc7c1deb3278bd239475723b
Chingis <chingisdev@gmail.com>
2024-10-04 19:17:51 +0300

    Add BarField enum and get_by_bar_field method
    Introduced the BarField enum to categorize data fields like OPEN, HIGH, LOW, CLOSE, and VOLUME. Implemented the get_by_bar_field method in InputData to fetch data based on these fields. Also, added a to_str method to convert BarField variants to string representations.

--- src/models/data.rs
+++ src/models/data.rs
@@ -8,7 +8,39 @@ pub struct InputData {
     pub volume: Option<Array1<f64>>,
 }
 
+impl InputData {
+    pub fn get_by_bar_field(&self, bar_field: &BarField) -> Option<&Array1<f64>> {
+        match bar_field {
+            BarField::OPEN => self.open.as_ref(),
+            BarField::HIGH => self.high.as_ref(),
+            BarField::LOW => self.low.as_ref(),
+            BarField::CLOSE => self.close.as_ref(),
+            BarField::VOLUME => self.volume.as_ref(),
+        }
+    }
+}
+
 pub enum OutputData {
     SingleSeries(Array1<f64>),
     MultiSeries(Array2<f64>)
+}
+
+pub enum BarField {
+    OPEN,
+    HIGH,
+    LOW,
+    CLOSE,
+    VOLUME,
+}
+
+impl BarField {
+    pub fn to_str(&self) -> &str {
+        match self {
+            BarField::OPEN => "OPEN",
+            BarField::HIGH => "HIGH",
+            BarField::LOW => "LOW",
+            BarField::CLOSE => "CLOSE",
+            BarField::VOLUME => "VOLUME",
+        }
+    }
 }
